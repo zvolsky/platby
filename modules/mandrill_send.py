c5062bfa57493558085d92789bdb921542d2c89f
zvolsky <zvolsky@seznam.cz>
2016-04-15 09:22:31 +0200

    mandrill -> sparkpost

--- modules/mandrill_send.py
+++ modules/mandrill_send.py
@@ -9,12 +9,15 @@ import base64
 import os
 import mimetypes
 
-import mandrill
+#import mandrill
+from sparkpost import SparkPost
 import vfp
 
 appdir = os.path.join(os.getcwd(), 'applications', 'platby')
-mandrill_key = vfp.filetostr(os.path.join(appdir,
-          'private', 'zvolsky_gmail_mandrill.smtp'))
+#mandrill_key = vfp.filetostr(os.path.join(appdir,
+#          'private', 'zvolsky_gmail_mandrill.smtp'))
+sparkpost_key = vfp.filetostr(os.path.join(appdir,
+          'private', 'sparkpost_zitranavylet_zvolsky.key')).strip()
 
 def mandrill_send(subject, txt, prilohy=[], prijemci=
       [{'email': 'mirek.zvolsky@gmail.com', 'name': u'Mirek Zvolský na Googlu'},
@@ -30,28 +33,41 @@ def mandrill_send(subject, txt, prilohy=[], prijemci=
     attachments = []
     for priloha in prilohy:
         attachments.append({
-                'content': base64.b64encode(open(priloha, 'rb').read()),
+                'filename': priloha,
                 'name': os.path.basename(priloha),
-                'type': mimetypes.guess_type(priloha) or 'text/plain'
+                'type': mimetypes.guess_type(priloha)[0] or 'text/plain'
         })
+    #pro mandrill bylo:
+    #'content': base64.b64encode(open(priloha, 'rb').read()), # pro mandrill bylo 'content'
+    #'type': mimetypes.guess_type(priloha) or 'text/plain'
+
+    stylDict = {styl: txt}
+    sp = SparkPost(sparkpost_key)
+    sp.transmissions.send(
+        recipients=[prijemce['email'] for prijemce in prijemci],
+        from_email=u'Společné Aktivity <spolecneaktivity@zitranavylet.cz>',
+        subject=subject,
+        attachments=attachments,
+        **stylDict
+    )
 
     # https://mandrillapp.com/api/docs/messages.python.html#method=send
-    m=mandrill.Mandrill(mandrill_key)
-    msg = {
-         'from_email': 'spolecneaktivityos@gmail.com',
-         'from_name': u'Společné Aktivity o.s.',
-         'subject': subject,
-         styl: txt,
-         'to': prijemci,
-         'attachments': attachments,
-    }
+    #m=mandrill.Mandrill(mandrill_key)
+    #msg = {
+    #     'from_email': 'spolecneaktivityos@gmail.com',
+    #     'from_name': u'Společné Aktivity o.s.',
+    #     'subject': subject,
+    #     styl: txt,
+    #     'to': prijemci,
+    #     'attachments': attachments,
+    #}
         # 'attachments': [{'content': 'ZXhhbXBsZSBmaWxl',
         #                 'name': 'myfile.txt', 'type': 'text/plain'}]
         #
         #    def filetobase64(self, inputfilename):
         # return base64.b64encode(open(inputfilename, 'rb').read())
         
-    m.messages.send(message=msg)
+    #m.messages.send(message=msg)
 
 # společné pro plánované maily
 #  -v controléru postak.py i ve scriptu pro odesílání scripts/send_plan_maily.py
